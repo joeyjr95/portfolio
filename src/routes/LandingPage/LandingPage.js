8b0357e69af29a453c226daa84f01349e4226e01
Joey Romo <joeyjr95@gmail.com>
2019-12-24 18:36:22 -0500

    tweaks

--- src/routes/LandingPage/LandingPage.js
+++ src/routes/LandingPage/LandingPage.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import "./LandingPage.scss";
 export default class LandingPage extends Component {
+
   render() {
+    const h2 = '{ Web Developer }'
     return (
       <div className="homepage-info">
         <div className="first-section">
           <h1> Joey Romo </h1>
           <img src="https://i.imgur.com/jEoyqgx.jpg" alt="joey romo" />
-          <h2> Web Developer </h2>
+          <h2>{h2}</h2>
         </div>
         <div className="second-section">
           <div className="h3-container">
