0f1c36e8d50e4ae99c960fbdaa376154ed5ca767
Joey Romo <joeyjr95@gmail.com>
2019-12-25 00:41:18 -0500

    done day 1

--- src/routes/Portfolio/Portfolio.js
+++ src/routes/Portfolio/Portfolio.js
@@ -12,7 +12,7 @@ export default class Portfolio extends Component {
                 title: 'Groop',
                 description: `Groop is a task manager for family, friends, or work. Organize tasks and members from multiple groups and view a scoreboard to effectively finish tasks done on time. Members accumulate points based on a finished task's priority level. Get status updates and weekly reminders with optional email notifications.`,
                 mImg: 'https://i.imgur.com/RyN8nbK.png',
-                img: '',
+                img: 'https://i.imgur.com/eUhL9aG.png',
                 stack: 'ReactJS, SCSS, Node, Express, Knex, PostgreSQL, Mocha, Chai',
                 live: 'https://grooplist.com/',
                 repo: "https://github.com/joeyjr95/groop-client",
@@ -23,7 +23,7 @@ export default class Portfolio extends Component {
                 title: 'Wortjäger',
                 description: ' Wortjäger is a React application that teaches users simple German words via spaced repetition. German words are displayed on the page and users are expected to provide the correct English translation of the word. Spaced repetition works by more frequently showing words that users get wrong and less frequently displaying words that users answer correctly.',
                 mImg: 'https://i.imgur.com/fimI56i.png',
-                img: '',
+                img: 'https://i.imgur.com/eUhL9aG.png',
                 stack: 'ReactJS, CSS, Node, Express, Knex, PostgreSQL, Mocha, Chai',
                 live: "https://william-joey-wortjager-app.now.sh",
                 repo: "https://github.com/thinkful-ei-gecko/william-joey-spaced-repetition-client",
@@ -34,7 +34,7 @@ export default class Portfolio extends Component {
                 title: 'How You Doin?',
                 description: `"How You Doin?" lets you keep track of how you're doing mentally and physically daily and keeps charts on the desktop app and averages to show you how you're progressing through the week and overall time of using the app. On the homepage, you have access to your data as well as two different charts helping you visualize your day to day progress as well as your overall progress.`,
                 mImg: "https://i.imgur.com/on46Q9J.png",
-                img: '',
+                img: 'https://i.imgur.com/eUhL9aG.png',
                 stack: 'ReactJS, CSS, Node, Express, Knex, PostgreSQL, Mocha, Chai',
                 live: "https://frontend.joeyjr95.now.sh",
                 repo: "https://github.com/joeyjr95/daily-wellness",
@@ -72,13 +72,18 @@ render(){
           ><h4>{project.title}</h4></a>
                <div className="project-body">
                <div className={collapssed[Number(project.id)] ? "toggleContent-open" : "toggleContent-closed"}>
+                   <div className="section-flex">
                 <p className="description">
                 {project.description}
                 </p>
-                 <img src={project.mImg} alt="website"/>      
+                
+                 <img src={window.innerWidth >= 1200 ? project.img : project.mImg} alt="website"/>     
+                 </div> 
                 <p className="stack">Tech Stack: {project.stack}</p>
-                <h5><Link to={project.live}>live project</Link></h5>
-                <h5><Link to={project.repo}>repo</Link></h5>
+                <div className="links">
+                <Link to={project.live}>live project</Link>
+                <Link to={project.repo}>repo</Link>
+                </div>
              </div>  
           
           
