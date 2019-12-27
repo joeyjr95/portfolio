91e00dc8162c045fa9ccba027530a89611a4ba4f
Joey Romo <joeyjr95@gmail.com>
2019-12-27 12:43:01 -0500

    functioning

--- src/routes/Portfolio/Portfolio.js
+++ src/routes/Portfolio/Portfolio.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 import React, { Component } from 'react';
-import './Portfolio.scss'
+import './Portfolio.scss';
+import SlideShow from './SlideShow';
 
 
 export default class Portfolio extends Component {
@@ -11,7 +12,7 @@ export default class Portfolio extends Component {
                 title: 'Groop',
                 description: `Groop is a task manager for family, friends, or work. Organize tasks and members from multiple groups and view a scoreboard to effectively finish tasks done on time. Members accumulate points based on a finished task's priority level. Get status updates and weekly reminders with optional email notifications.`,
                 mImg: 'https://i.imgur.com/RyN8nbK.png',
-                img: 'https://i.imgur.com/eUhL9aG.png',
+                img: ['https://i.imgur.com/eUhL9aG.png','https://i.imgur.com/19liZkD.png','https://i.imgur.com/kBNaIKL.png'],
                 stack: 'ReactJS, SCSS, Node, Express, Knex, PostgreSQL, Mocha, Chai',
                 live: 'https://grooplist.com/',
                 repo: "https://github.com/joeyjr95/groop-client",
@@ -22,7 +23,7 @@ export default class Portfolio extends Component {
                 title: 'Wortjäger',
                 description: ' Wortjäger is a React application that teaches users simple German words via spaced repetition. German words are displayed on the page and users are expected to provide the correct English translation of the word. Spaced repetition works by more frequently showing words that users get wrong and less frequently displaying words that users answer correctly.',
                 mImg: 'https://i.imgur.com/fimI56i.png',
-                img: 'https://i.imgur.com/19liZkD.png',
+                img: ['https://i.imgur.com/eUhL9aG.png','https://i.imgur.com/19liZkD.png','https://i.imgur.com/kBNaIKL.png'],
                 stack: 'ReactJS, CSS, Node, Express, Knex, PostgreSQL, Mocha, Chai',
                 live: "https://william-joey-wortjager-app.now.sh",
                 repo: "https://github.com/thinkful-ei-gecko/william-joey-spaced-repetition-client",
@@ -33,7 +34,7 @@ export default class Portfolio extends Component {
                 title: 'How You Doin?',
                 description: `"How You Doin?" lets you keep track of how you're doing mentally and physically daily and keeps charts on the desktop app and averages to show you how you're progressing through the week and overall time of using the app. On the homepage, you have access to your data as well as two different charts helping you visualize your day to day progress as well as your overall progress.`,
                 mImg: "https://i.imgur.com/on46Q9J.png",
-                img: 'https://i.imgur.com/kBNaIKL.png',
+                img: ['https://i.imgur.com/eUhL9aG.png','https://i.imgur.com/19liZkD.png','https://i.imgur.com/kBNaIKL.png'],
                 stack: 'ReactJS, CSS, Node, Express, Knex, PostgreSQL, Mocha, Chai',
                 live: "https://frontend.joeyjr95.now.sh",
                 repo: "https://github.com/joeyjr95/daily-wellness",
@@ -77,8 +78,7 @@ render(){
                 {project.description}
                 </p>
                 
-                 <img src={project.img} alt="website"/>
-                 <button>click me</button>     
+                 <SlideShow images={project.img}/>     
                  </div> 
                 <p className="stack">Tech Stack: {project.stack}</p>
                 <div className="links">
